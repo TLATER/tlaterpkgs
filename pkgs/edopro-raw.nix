4ae6d7365770eb739d50f6a5823dc222b57e5746
Tristan Daniël Maat <tm@tlater.net>
2022-02-27 23:03:01 +0000

    edopro: Stop patching the raw package
    This isn't necessary, we're using buildFHSUserEnv (edopro struggles
    with libGL even after being patched).

--- pkgs/edopro-raw.nix
+++ pkgs/edopro-raw.nix
@@ -1,22 +1,7 @@
-{ sources, stdenv, autoPatchelfHook, alsa-lib, gccForLibs, libGL, mono, udev
-, xorg }:
+{ sources, stdenv }:
 
 stdenv.mkDerivation {
   inherit (sources.edopro) pname version src;
-
-  nativeBuildInputs = [ autoPatchelfHook ];
-
-  buildInputs = [
-    gccForLibs.lib
-    alsa-lib
-    libGL
-    mono
-    udev
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXxf86vm
-  ];
 
   # Disable client updates
   patchPhase = ''
