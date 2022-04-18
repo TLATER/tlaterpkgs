4743d3111af6674114f6e40f5196841a8ff145fd
Tristan Daniël Maat <tm@tlater.net>
2022-04-18 04:49:51 +0100

    Update inputs

--- flake.nix
+++ flake.nix
@@ -1,36 +1,39 @@
 {
   description = "A collection of somewhat specialized nix packages";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = import ./pkgs {inherit pkgs;};
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        packages = import ./pkgs { inherit pkgs; };
-        apps = let packages = self.packages.${system};
-        in {
-          "EonTimer" = {
-            type = "app";
-            program = "${packages.eontimer}/bin/EonTimer";
-          };
+    apps.${system} = let
+      packages = self.packages.${system};
+    in {
+      "EonTimer" = {
+        type = "app";
+        program = "${packages.eontimer}/bin/EonTimer";
+      };
 
-          "PokeFinder" = {
-            type = "app";
-            program = "${packages.pokefinder}/bin/PokeFinder";
-          };
+      "PokeFinder" = {
+        type = "app";
+        program = "${packages.pokefinder}/bin/PokeFinder";
+      };
 
-          "gcs" = {
-            type = "app";
-            program = "${packages.gcs}/bin/gcs";
-          };
+      "gcs" = {
+        type = "app";
+        program = "${packages.gcs}/bin/gcs";
+      };
 
-          "EDOPro" = {
-            type = "app";
-            program = "${packages.edopro}/bin/edopro";
-          };
-        };
+      "EDOPro" = {
+        type = "app";
+        program = "${packages.edopro}/bin/edopro";
+      };
+    };
 
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ nvfetcher ]; };
-      });
+    devShells.${system}.default = pkgs.mkShell {buildInputs = with pkgs; [nvfetcher];};
+  };
 }
