21887164737da01164b2a9fc3bf88549d52f77ce
Tristan Daniël Maat <tm@tlater.net>
2022-08-30 03:21:43 +0100

    Update gcs package

--- pkgs/gcs.nix
+++ pkgs/gcs.nix
@@ -1,9 +1,17 @@
-{ sources, lib, stdenv, jdk17, jre, makeWrapper, wrapGAppsHook }:
-
+{
+  sources,
+  lib,
+  stdenv,
+  jdk17,
+  makeWrapper,
+  wrapGAppsHook,
+  glib,
+}:
 stdenv.mkDerivation rec {
   inherit (sources.gcs) pname version src;
 
-  nativeBuildInputs = [ jdk17 jre.gtk3 makeWrapper wrapGAppsHook ];
+  nativeBuildInputs = [jdk17 makeWrapper wrapGAppsHook];
+  buildInputs = [glib];
 
   dontConfigure = true;
   dontInstall = true;
@@ -14,14 +22,14 @@ stdenv.mkDerivation rec {
     ./bundle.sh --unpackaged
   '';
 
-  preFixup = let v = lib.removePrefix "v" version;
+  preFixup = let
+    v = lib.removePrefix "v" version;
   in ''
     mkdir -p $out/{bin,share}
     cp -r out/dist/modules $out/share/java
 
-    makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
-    makeWrapper ${jre}/bin/java $out/bin/gcs-${version} \
-        ''${makeWrapperArgs[@]} \
+    makeWrapper ${jdk17}/bin/java $out/bin/gcs \
+        ''${gappsWrapperArgs[@]} \
           --add-flags "-cp $out/share/java/com.lowagie.text-2.1.7.jar -jar $out/share/java/com.trollworks.gcs-${v}.jar"
   '';
 
