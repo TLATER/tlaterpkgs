5cbc3de6d052771b9f441687e73db2768794cbd0
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2022-11-15 21:20:04 +0000

    PokeFinder: Fix package

--- pkgs/pokefinder.nix
+++ pkgs/pokefinder.nix
@@ -3,20 +3,24 @@
   lib,
   stdenv,
   cmake,
-  libsForQt5,
+  qt6,
+  python3,
 }: let
-  inherit (libsForQt5.qt5) qtbase qttools wrapQtAppsHook;
+  inherit (qt6) qtbase qttools wrapQtAppsHook;
   inherit (sources) pokefinder;
 in
   stdenv.mkDerivation {
     inherit (pokefinder) pname version src;
     buildInputs = [qtbase];
-    nativeBuildInputs = [cmake qttools wrapQtAppsHook];
+    depsBuildBuild = [cmake qttools];
+    nativeBuildInputs = [python3 wrapQtAppsHook];
 
-    # Fix a bug in the codebase
-    patchPhase = ''
-      substituteInPlace Source/Core/Util/EncounterSlot.cpp --replace size_t std::size_t
-      substituteInPlace Source/Core/Util/IVChecker.cpp --replace size_t std::size_t
+    patches = [
+      ../fix-size_t.patch
+    ];
+
+    postPatch = ''
+      patchShebangs Source/Core/Resources/embed.py
     '';
 
     installPhase = ''
